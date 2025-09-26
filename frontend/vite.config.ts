64772ce45051b783145c03a17e6aa2f94e63efdf
sddhantjaiii <122254271+sddhantjaiii@users.noreply.github.com>
2025-09-26 13:56:04 +0530

    test3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: '.',
-  base: '/',
+  base: './',
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx']
   },
